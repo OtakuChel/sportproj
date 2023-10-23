5abab4373b78423863e8a71dc39b569796eaa574
OtakuChel <relz123@mail.ru>
2023-10-23 20:33:13 +0300

    Создал приложение для счета калорий

--- sport_project/sport_project/urls.py
+++ sport_project/sport_project/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('trainning_app.urls')),
+    path('', include('calorie_app.urls')),
 ]
