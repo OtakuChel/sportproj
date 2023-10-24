ee67f13626cb80da391e3259af8321efe1b726fc
OtakuChel <relz123@mail.ru>
2023-10-24 21:12:30 +0300

    Начал делать приложение, показывающее калорийность блюда

--- sport_project/calorie_app/views.py
+++ sport_project/calorie_app/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 
+
+#SZjrtFKYqN0DGqQgCdEGiw==qguXA82YiA66F8Pl
 # Create your views here.
 
 def home(request):
 
-    return render(request, 'home.html')
+    import requests
+    import json
+    if request.method == 'POST':
+        query = request.POST['query']
+        api_url = 'https://api.api-ninjas.com/v1/nutrition?query='
+        api_request = requests.get(api_url + query, headers={'X-Api-Key': 'SZjrtFKYqN0DGqQgCdEGiw==qguXA82YiA66F8Pl'})
+        try:
+            api = json.loads(api_request.content)
+            print(api_request.content)
+        except Exception as e:
+            api = 'oops! Error'
+            print(e)
+        return render(request, 'calorie_app/hom.html', context={'api': api})
+    else:
+        return render(request, 'calorie_app/hom.html', context={'query': 'Введите корректный запрос'})
